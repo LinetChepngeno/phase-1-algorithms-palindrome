509a4d6c36819977bbcebb20b6f3934494f8576d
Linet Chepngeno <linetmutai@gmail.com>
2024-01-14 16:03:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+function reverse(word) {
+  // pseudocode 'abc' returns 'cba'
+  // const wordArray = word.split("");
+  // const reversedWordArray = wordArray.reverse();
+  // const reversedWord = reversedWordArray.join("");
+  // return reversedWord;
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  pseudocode means a word which is the same as the word in reverse, it should return true.
+  
+  reverse the input string
+
+  if the input is the same as the reversed input
+    return true
+  else
+    return false
 */
 
 /*
   Add written explanation of your solution here
+
+  This code is used to test pseudocode for words
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +41,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
 }
 
 module.exports = isPalindrome;
